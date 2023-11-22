03754ac63109090cbda94d5bccf41a1db9634b33
Murali Krishnasamy <mukrishn@redhat.com>
2023-11-22 15:38:34 -0500

    fixed shell checks

--- utils/loop_script.sh
+++ utils/loop_script.sh
@@ -13,7 +13,7 @@ OPERATOR_ROLES_PREFIX="${OPERATOR_ROLES_PREFIX:loop-role}"
 export AWS_REGION="${AWS_REGION:-us-east-2}"
 ###
 
-exec 3>&1 1>>${LOG_FILE} 2>&1
+exec 3>&1 1>>"${LOG_FILE}" 2>&1
 
 log() {
   message=$1
@@ -23,7 +23,7 @@ log() {
 
 _delete_clusters() {
   log "INFO: Captured Control-C key, deleting all clusters with ${1} seed" | tee /dev/fd/3
-  for CLUSTER in $(rosa list clusters | grep $1 | awk '{print $1}'); do
+  for CLUSTER in $(rosa list clusters | grep "$1" | awk '{print $1}'); do
     log "INFO: Deleting cluster ${CLUSTER}" | tee /dev/fd/3
     _delete_cluster "${CLUSTER}"
     log "INFO: Cluster ${CLUSTER} deleted" | tee /dev/fd/3
@@ -34,23 +34,24 @@ _delete_clusters() {
 _create_cluster() {
   log "INFO: Creating cluster $1" | tee /dev/fd/3
   # Timeout of 15 minutes for creating the cluster (3 times of normal execution)
-  timeout --foreground -k 900 900 rosa create cluster -c $1 --replicas 3 --hosted-cp --sts --mode auto -y --watch --multi-az --subnet-ids $2 --properties $3 --oidc-config-id ${OIDC_CONFIG_ID} --operator-roles-prefix ${OPERATOR_ROLES_PREFIX} --compute-machine-type m5.xlarge --version 4.14.2 || (log "ERROR: Failed to create cluster $1 after 15 minutes" && return 1)
-  log "INFO: Cluster $1 created" 3>&1 1>>${LOG_FILE} | tee /dev/fd/3
+  timeout --foreground -k 900 900 rosa create cluster -c "$1" --replicas 3 --hosted-cp --sts --mode auto -y --watch --multi-az --subnet-ids "$2" --properties "$3" --oidc-config-id "${OIDC_CONFIG_ID}" --operator-roles-prefix "${OPERATOR_ROLES_PREFIX}" --compute-machine-type m5.xlarge --version 4.14.2 || (log "ERROR: Failed to create cluster $1 after 15 minutes" && return 1)
+  log "INFO: Cluster $1 created" 3>&1 1>>"${LOG_FILE}" | tee /dev/fd/3
 }
 
 _delete_cluster(){
   # Timeout of 60 minutes for cleaning the cluster (3 times of normal execution)
-  timeout --foreground -k 3600 3600 rosa delete cluster -c $1 -y --watch|| (log "ERROR: Failed to delete cluster $1 after 30 minutes" && return 1)
+  timeout --foreground -k 3600 3600 rosa delete cluster -c "$1" -y --watch|| (log "ERROR: Failed to delete cluster $1 after 30 minutes" && return 1)
 #  # Timeout of 5 minutes for Roles and OIDC
-#  timeout --foreground -k 300 300 rosa delete operator-roles -c $1 -m auto -y || (log "ERROR: Failed to delete operator roles of cluster $1 after 5 minutes" && return 1)
-#  timeout --foreground -k 300 300 rosa delete oidc-provider -c $1 -m auto -y || (log "ERROR: Failed to delete OIDC providers of cluster $1 after 5 minutes" && return 1)
+#  timeout --foreground -k 300 300 rosa delete operator-roles -c "$1" -m auto -y || (log "ERROR: Failed to delete operator roles of cluster $1 after 5 minutes" && return 1)
+#  timeout --foreground -k 300 300 rosa delete oidc-provider -c "$1" -m auto -y || (log "ERROR: Failed to delete OIDC providers of cluster $1 after 5 minutes" && return 1)
 }
 
 CLUSTER_INDEX=0
-EXECUTION_SEED="${CLUSTER_NAME_SEED}"-$(tr -dc 'a-z' < /dev/urandom | fold -w 3 | head -n 1)
+EXECUTION_SEED="${CLUSTER_NAME_SEED}"-$(tr -dc '[:lower:]' < /dev/urandom | fold -w 3 | head -n 1)
 while true; do
-  CLUSTERS_CREATED_LIST=($(rosa list clusters | grep "${EXECUTION_SEED}" | awk '{print $2}'))
-  CLUSTERS_CREATED_TOTAL=$(rosa list clusters | grep "${EXECUTION_SEED}" | wc -l)
+  CLUSTERS_CREATED_LIST=()
+  while IFS='' read -r line; do CLUSTERS_CREATED_LIST+=("$line"); done < <(rosa list clusters | grep "${EXECUTION_SEED}" | awk '{print $2}')
+  CLUSTERS_CREATED_TOTAL="$(rosa list clusters | grep -c "${EXECUTION_SEED}")"
   if [ "${CLUSTERS_CREATED_TOTAL}" -lt  "${NUMBER_OF_CLUSTERS}" ] ; then
     log "INFO: Clusters created (${CLUSTERS_CREATED_TOTAL}) under threshold (${NUMBER_OF_CLUSTERS}), creating a new one" | tee /dev/fd/3
     ((CLUSTER_INDEX+=1))
